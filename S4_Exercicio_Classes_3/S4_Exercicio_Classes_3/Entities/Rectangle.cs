bddc0c0305bd9f85df06f5b7da85457ad8f8f954
Daniel Montenegro <danielmmend@gmail.com>
2022-04-08 17:32:28 -0300

    Sessão 4 (Classes, Atributos, Métodos e Membros Estáticos) - Exercicios 3

---
+++ S4_Exercicio_Classes_3/S4_Exercicio_Classes_3/Entities/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace S4_Exercicio_Classes_3.Entities
+{
+    class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Area()
+        {
+            return Width * Height;
+        }
+        public double Perimeter()
+        {
+            return (Width * 2) + (Height * 2);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Width * Width + Height * Height);
+        }
+    }
+}
---
+++ S4_Exercicio_Classes_3/S4_Exercicio_Classes_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using S4_Exercicio_Classes_3.Entities;
+
+namespace S4_Exercicio_Classes_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            double rWidth = double.Parse(Console.ReadLine());
+            double rHeight = double.Parse(Console.ReadLine());
+            Rectangle ret = new Rectangle(rWidth, rHeight);
+
+            Console.WriteLine($"AREA = {ret.Area():F2}");
+            Console.WriteLine($"PERIMETRO = {ret.Perimeter():F2}");
+            Console.WriteLine($"DIAGONAL = {ret.Diagonal():F2}");
+        }
+    }
+}
