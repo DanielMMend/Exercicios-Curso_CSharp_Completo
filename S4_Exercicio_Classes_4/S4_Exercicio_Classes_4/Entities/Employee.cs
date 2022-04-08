7b7a313279d71cb8e870a314c029964f7c6ff119
Daniel Montenegro <danielmmend@gmail.com>
2022-04-08 17:51:03 -0300

    Sessão 4 (Classes, Atributos, Métodos e Membros Estáticos) - Exercicio 4

---
+++ S4_Exercicio_Classes_4/S4_Exercicio_Classes_4/Entities/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace S4_Exercicio_Classes_4.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public double GrossSalary { get; set; }
+        public double Tax { get; set; }
+
+        public Employee(string name, double grossSalary, double tax)
+        {
+            Name = name;
+            GrossSalary = grossSalary;
+            Tax = tax;
+        }
+
+        public double NetSalary()
+        {
+            return GrossSalary - Tax;
+        }
+        public void IncreaseSalary(double percentage)
+        {
+            GrossSalary = GrossSalary + ((GrossSalary * percentage) / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, $ {NetSalary():F2}";
+        }
+    }
+}
---
+++ S4_Exercicio_Classes_4/S4_Exercicio_Classes_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using S4_Exercicio_Classes_4.Entities;
+
+namespace S4_Exercicio_Classes_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nome: ");
+            string eName = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            double eGrossSalary = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            double eTax = double.Parse(Console.ReadLine());
+            Employee emp = new Employee(eName, eGrossSalary, eTax);
+
+            Console.WriteLine($"\nFuncionário: {emp}");
+
+            Console.Write("\nDigite a porcentagem para aumentar o salário: ");
+            double percentage = double.Parse(Console.ReadLine());
+            emp.IncreaseSalary(percentage);
+
+            Console.WriteLine($"Dados atualizados: {emp}");
+
+        }
+    }
+}
