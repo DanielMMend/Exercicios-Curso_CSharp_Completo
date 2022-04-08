35ad2ac7c486d0d6fcf4af6ab0cfa06e6285bb7c
Daniel Montenegro <danielmmend@gmail.com>
2022-04-08 18:18:41 -0300

    Sessão 4 (Classes, Atributos, Métodos e Membros Estáticos) - Exercicio 5

---
+++ S4_Exercicio_Classes_5/S4_Exercicio_Classes_5/Entities/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace S4_Exercicio_Classes_5.Entities
+{
+    class Student
+    {
+        public double Grade1 { get; set; }
+        public double Grade2 { get; set; }
+        public double Grade3 { get; set; }
+
+        public Student(double grade1, double grade2, double grade3)
+        {
+            Grade1 = grade1;
+            Grade2 = grade2;
+            Grade3 = grade3;
+        }
+
+        public double FinalGrade()
+        {
+            return Grade1 + Grade2 + Grade3;
+        }
+        public void Approval()
+        {
+            if (FinalGrade() < 60.0)
+            {
+                Console.WriteLine("REPROVADO");
+            }
+            else
+            {
+                Console.WriteLine("APROVADO");
+            }
+        }
+        public void RemainingGrade()
+        {
+            if (FinalGrade() < 60.0)
+            {
+                Console.WriteLine($"FALTARAM {60.0 - FinalGrade():F2} PONTOS");
+            }
+        }
+    }
+}
---
+++ S4_Exercicio_Classes_5/S4_Exercicio_Classes_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using S4_Exercicio_Classes_5.Entities;
+
+namespace S4_Exercicio_Classes_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nome do aluno: ");
+            Console.WriteLine("Digite as três notas do aluno:");
+            double sGrade1 = double.Parse(Console.ReadLine());
+            double sGrade2 = double.Parse(Console.ReadLine());
+            double sGrade3 = double.Parse(Console.ReadLine());
+            Student std = new Student(sGrade1, sGrade2, sGrade3);
+
+            Console.WriteLine($"\nNOTA FINAL: {std.FinalGrade():F2}");
+            std.Approval();
+            std.RemainingGrade();
+        }
+    }
+}
