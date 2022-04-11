0025d4ab2e599a8d3372bc21e29b9ce08c9bacc8
Daniel Montenegro <danielmmend@gmail.com>
2022-04-11 16:16:55 -0300

    Sessão 4 (Classes, Atributos, Métodos e Membros Estáticos) - Exercicio 6

---
+++ S4_Exercicio_Classes_6/S4_Exercicio_Classes_6/Entities/CurrencyConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace S4_Exercicio_Classes_6.Entities
+{
+    class CurrencyConverter
+    {
+        public double DollarQuote { get; set; }
+        public static double IOF = 1.06;
+
+        public CurrencyConverter(double dollarQuote)
+        {
+            DollarQuote = dollarQuote;
+        }
+
+        public double FinalValue(double dollarAmount)
+        {
+            return (dollarAmount * DollarQuote) * IOF;
+        }
+    }
+}
---
+++ S4_Exercicio_Classes_6/S4_Exercicio_Classes_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using S4_Exercicio_Classes_6.Entities;
+
+namespace S4_Exercicio_Classes_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double dq = double.Parse(Console.ReadLine());
+            Console.Write("Quantos dólares você vai comprar? ");
+            double da = double.Parse(Console.ReadLine());
+            CurrencyConverter cc = new CurrencyConverter(dq);
+            Console.WriteLine($"Valor a ser pago em reais = {cc.FinalValue(da):F2}");
+        }
+    }
+}
