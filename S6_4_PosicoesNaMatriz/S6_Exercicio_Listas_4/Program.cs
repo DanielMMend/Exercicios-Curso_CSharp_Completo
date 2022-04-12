8ff2e6a02eefaf5c1f453ad4b76d59b39aa4ffe6
Daniel Montenegro <danielmmend@gmail.com>
2022-04-12 18:17:31 -0300

    Exercicio 4 - Sessão 6 (Arrays, Listas e Comportamento de Memória)

---
+++ S6_4_PosicoesNaMatriz/S6_Exercicio_Listas_4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace S6_Exercicio_Listas_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int m = int.Parse(s[0]);
+            int n = int.Parse(s[1]);
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] s2 = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(s2[j]);
+                }
+            }
+
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine($"Position: {i}, {j}");
+
+                        if (j != 0)
+                        {
+                            Console.WriteLine($"Left: {mat[i, j - 1]}");
+                        }
+                        if (j != n - 1)
+                        {
+                            Console.WriteLine($"Right: {mat[i, j + 1]}");
+                        }
+                        if (i != 0)
+                        {
+                            Console.WriteLine($"Up: {mat[i - 1, j]}");
+                        }
+                        if (i != m - 1)
+                        {
+                            Console.WriteLine($"Down: {mat[i + 1, j]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ S6_4_PosicoesNaMatriz/readme.md
@@ -0,0 +1,11 @@
+# Exercício 4
+
+Fazer um programa para ler dois números inteiros M e N, e depois ler uma matriz de M linhas por N colunas contendo números inteiros,podendo haver repetições. 
+
+Em seguida, ler um número inteiro X que pertence à matriz.
+
+Para cada ocorrência de X, mostrar os valores à esquerda, acima, à direita e abaixo de X, quando houver, conforme
+exemplo:
+
+![](https://uploaddeimagens.com.br/images/003/826/231/original/imagem_2022-04-12_181422357.png?1649798131)
+
