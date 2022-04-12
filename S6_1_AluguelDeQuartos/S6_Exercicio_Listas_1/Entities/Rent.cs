4c40d349a8c01ca03f3b1199b7d5b62e2b8522db
Daniel Montenegro <danielmmend@gmail.com>
2022-04-12 16:15:29 -0300

    Exercicio 1 - Sessão 6 (Arrays, Listas e Comportamento de Memória)

---
+++ S6_1_AluguelDeQuartos/S6_Exercicio_Listas_1/Entities/Rent.cs
@@ -0,0 +1,21 @@
+﻿namespace S6_Exercicio_Listas_1.Entities
+{
+    class Rent
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+        public Rent(string name, string email, int room)
+        {
+            Name = name;
+            Email = email;
+            Room = room;
+        }
+
+        public override string ToString()
+        {
+            return $"{Room}: {Name}, {Email}";
+        }
+    }
+}
---
+++ S6_1_AluguelDeQuartos/S6_Exercicio_Listas_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using S6_Exercicio_Listas_1.Entities;
+
+namespace S6_Exercicio_Listas_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rent[] rents = new Rent[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"\nAluguel #{i + 1}:");
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int room = int.Parse(Console.ReadLine());
+
+                Rent rent = new Rent(name, email, room);
+
+                rents[room - 1] = rent;
+            }
+
+            Console.WriteLine("Quartos ocupados:");
+            foreach (Rent r in rents)
+            {
+                if (r != null)
+                {
+                    Console.WriteLine(r);
+                }
+            }
+        }
+    }
+}
