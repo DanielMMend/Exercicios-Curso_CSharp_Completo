f36858a698a451108c0d093ce1f163181ec9559b
Daniel Montenegro <danielmmend@gmail.com>
2022-04-12 17:42:31 -0300

    Exercicio 3 - Sessão 6 (Arrays, Listas e Comportamento de Memória)

---
+++ S6_3_DiagonalPrincipal/S6_Exercicio_Listas_3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace S6_Exercicio_Listas_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            Console.Write("\nMain diagonal: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(mat[i, j] + " ");
+                    }
+                }
+            }
+
+            int negNumbers = 0;
+            foreach (int x in mat)
+            {
+                if (x < 0)
+                {
+                    negNumbers = negNumbers + 1;
+                }
+            }
+            Console.WriteLine($"\nNegative numbers = {negNumbers}");
+        }
+    }
+}
---
+++ S6_3_DiagonalPrincipal/readme.md
@@ -0,0 +1,6 @@
+# Exercício 3
+
+Fazer um programa para ler um número inteiro N e uma matriz de ordem N contendo números inteiros. Em seguida, mostrar a diagonal principal e a quantidade de valores negativos da matriz, conforme o exemplo abaixo:
+
+![](https://uploaddeimagens.com.br/images/003/826/182/original/Screenshot_2.png?1649796031)
+
