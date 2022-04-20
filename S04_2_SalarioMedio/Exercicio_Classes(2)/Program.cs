6500c08b8c856f5ed5a69aea11d3e48aaf8ef13a
Daniel Montenegro <danielmmend@gmail.com>
2022-04-20 14:14:42 -0300

    Exercicio 2 - Sessão 4 (Classes, Atributos, Métodos e Membros Estáticos)

--- S04_2_SalarioMedio/Exercicio_Classes(2)/Program.cs
+++ S04_2_SalarioMedio/Exercicio_Classes(2)/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using Exercicio_Classes_2_.Entities;
 
 namespace Exercicio_Classes_2_
@@ -11,17 +12,17 @@ namespace Exercicio_Classes_2_
             Console.Write("Nome: ");
             string e1Name = Console.ReadLine();
             Console.Write("Salário: ");
-            double e1Salary = double.Parse(Console.ReadLine());
+            double e1Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Employee emp1 = new Employee(e1Name, e1Salary);
 
             Console.WriteLine("\nDados do segundo funcionário:");
             Console.Write("Nome: ");
             string e2Name = Console.ReadLine();
             Console.Write("Salário: ");
-            double e2Salary = double.Parse(Console.ReadLine());
+            double e2Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Employee emp2 = new Employee(e2Name, e2Salary);
 
-            Console.WriteLine($"\nSalário médio = {emp1.AverageSalary(emp2):F2}");
+            Console.WriteLine($"\nSalário médio = {emp1.AverageSalary(emp2).ToString("F2", CultureInfo.InvariantCulture)}");
         }
     }
 }
