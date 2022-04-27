34901319e5764dc9f6b97d0c230941972736884d
Daniel Montenegro <danielmmend@gmail.com>
2022-04-27 18:04:08 -0300

    Exercicio 4 - Sessão 15 (Generics, Set e Dictionary)

---
+++ S15_4_ContagemDeVotos/S15_Exercicio_Generics_4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace S15_Exercicio_Generics_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Dictionary<string, int> dic = new Dictionary<string, int>();
+                Console.Write("Enter file full path: ");
+                string sourcePath = Console.ReadLine();
+
+                using (StreamReader sr = File.OpenText(sourcePath))
+                {
+                    while (sr.EndOfStream == false)
+                    {
+                        string[] s = sr.ReadLine().Split(',');
+                        string name = s[0];
+                        int votes = int.Parse(s[1]);
+
+                        if (dic.ContainsKey(name) == false)
+                        {
+                            dic.Add(name, votes);
+                        }
+                        else
+                        {
+                            dic[name] = dic[name] + votes;
+                        }
+                    }
+                }
+
+                foreach (KeyValuePair<string, int> item in dic)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ S15_4_ContagemDeVotos/readme.md
@@ -0,0 +1,3 @@
+# Exercício 4
+
+![](https://uploaddeimagens.com.br/images/003/846/116/original/Sem_t%C3%ADtulo.png?1651093468)
