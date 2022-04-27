fa28dca0e49eafa409ab8d40d2b904c1346491e1
Daniel Montenegro <danielmmend@gmail.com>
2022-04-27 17:25:35 -0300

    Exercicio 3 - Sessão 15 (Generics, Set e Dictionary)

---
+++ S15_Exercicio_Generics_3/S15_Exercicio_Generics_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace S15_Exercicio_Generics_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            Console.Write("How many students for course A? ");
+            int nA = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= nA; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                set.Add(x);
+            }
+
+            Console.Write("\nHow many students for course B? ");
+            int nB = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= nB; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                set.Add(x);
+            }
+
+            Console.Write("\nHow many students for course C? ");
+            int nC = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= nC; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                set.Add(x);
+            }
+
+            Console.WriteLine($"\nTotal students: {set.Count}");
+        }
+    }
+}
---
+++ S15_Exercicio_Generics_3/readme.md
@@ -0,0 +1,3 @@
+# Exercício 3
+
+![](https://uploaddeimagens.com.br/images/003/846/033/original/Sem_t%C3%ADtulo.png?1651091132)
