9e81fbf06d904f86dc6760a6c4fc2777ed8ee75a
Daniel Montenegro <danielmmend@gmail.com>
2022-04-29 18:48:57 -0300

    Exercicio 5 - Sessão 17 (Expressões Lambda, Delegates e LINQ)

---
+++ S17_5_FiltrarFuncionarios/S17_Exercicio_LINQ_5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using S17_Exercicio_LINQ_5.Entities;
+
+namespace S17_Exercicio_LINQ_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+            string sourcePatch = @"C:\Users\danie\source\repos\Curso CSharp Completo - Exercicios\S17_Exercicio_LINQ_5\S17_Exercicio_LINQ_5\File\file.csv";
+
+            using (StreamReader sr = File.OpenText(sourcePatch))
+            {
+                while (sr.EndOfStream == false)
+                {
+                    string[] s = sr.ReadLine().Split(',');
+                    string eName = s[0];
+                    string eEmail = s[1];
+                    double eSalary = double.Parse(s[2], CultureInfo.InvariantCulture);
+
+                    Employee emp = new Employee(eName, eEmail, eSalary);
+                    list.Add(emp);
+                }
+            }
+
+            Console.Write("Enter salary: ");
+            double enterSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine($"\nEmail of people whose salary is more than {enterSalary.ToString("F2", CultureInfo.InvariantCulture)}:");
+
+            var emails = list.Where(p => p.Salary > enterSalary).OrderBy(p => p.Email).Select(p => p.Email);
+            foreach (string s in emails)
+            {
+                Console.WriteLine(s);
+            }
+
+            var sum = list.Where(p => p.Name[0] == 'M').Select(p => p.Salary).Sum();
+            Console.WriteLine($"\nSum of salary of people whose name starts with 'M': {sum.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ S17_5_FiltrarFuncionarios/readme.md
@@ -0,0 +1,11 @@
+# Exercício 5
+
+Fazer um programa para ler um conjunto de produtos a partir de um Fazer um programa para ler os dados (nome, email e salário) de funcionários a partir de um arquivo em formato .csv.
+
+Em seguida mostrar, em ordem alfabética, o email dos funcionários cujo salário seja superior a um dado valor fornecido pelo usuário.
+
+Mostrar também a soma dos salários dos funcionários cujo nome começa com a letra 'M'.
+
+Veja exemplo abaixo:
+
+![](https://i.ibb.co/8skqXv3/imagem-2022-04-29-184638467.png)
