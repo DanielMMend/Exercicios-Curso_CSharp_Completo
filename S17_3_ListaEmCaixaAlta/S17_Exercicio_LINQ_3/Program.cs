ddc7b6f9e66757aa298bbaa69b08c683f55d4a79
Daniel Montenegro <danielmmend@gmail.com>
2022-04-29 17:19:43 -0300

    Exercicio 3 - Sessão 17 (Expressões Lambda, Delegates e LINQ)

---
+++ S17_3_ListaEmCaixaAlta/S17_Exercicio_LINQ_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using S17_Exercicio_LINQ_3.Entities;
+using System.Linq;
+
+namespace S17_Exercicio_LINQ_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            List<string> list2 = list.Select(p => p.Name.ToUpper()).ToList();
+
+            foreach (string s in list2)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ S17_3_ListaEmCaixaAlta/readme.md
@@ -0,0 +1,13 @@
+# Exercício 3
+
+Fazer um programa que, a partir de uma lista de produtos, gere uma nova lista contendo os nomes dos produtos em caixa alta.
+
+![](https://i.ibb.co/2cmDnH6/imagem-2022-04-29-154758191.png)
+
+**Exemplo:**
+* * *
+TV<br/>
+MOUSE<br/>
+TABLET<br/>
+HD CASE<br/>
+* * *
