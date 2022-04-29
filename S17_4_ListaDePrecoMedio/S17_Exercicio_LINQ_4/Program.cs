356aa13fefb69304c2f0a64f9370990176fc8d0c
Daniel Montenegro <danielmmend@gmail.com>
2022-04-29 18:11:07 -0300

    Exercicio 4 - Sessão 17 (Expressões Lambda, Delegates e LINQ)

---
+++ S17_4_ListaDePrecoMedio/S17_Exercicio_LINQ_4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using S17_Exercicio_LINQ_4.Entities;
+
+namespace S17_Exercicio_LINQ_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<Product> list = new List<Product>();
+
+                Console.Write("Enter full file path: ");
+                string sourcePatch = Console.ReadLine();
+
+                using (StreamReader sr = File.OpenText(sourcePatch))
+                {
+                    while (sr.EndOfStream == false)
+                    {
+                        string[] s = sr.ReadLine().Split(',');
+                        string pName = s[0];
+                        double pPrice = double.Parse(s[1], CultureInfo.InvariantCulture);
+
+                        Product p = new Product(pName, pPrice);
+                        list.Add(p);
+                    }
+                }
+
+                var avg = list.Select(p => p.Price).Average();
+                Console.WriteLine($"Average price: {avg.ToString("F2", CultureInfo.InvariantCulture)}");
+
+                var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+                foreach (String s in names)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ S17_4_ListaDePrecoMedio/readme.md
@@ -0,0 +1,9 @@
+# Exercício 4
+
+Fazer um programa para ler um conjunto de produtos a partir de um arquivo em formato .csv (suponha que exista pelo menos um produto).
+
+Em seguida mostrar o preço médio dos produtos.
+
+Depois, mostrar os nomes, em ordem decrescente, dos produtos que possuem preço inferior ao preço médio, conforme o exemplo abaixo:
+
+![](https://i.ibb.co/bm3hc9J/imagem-2022-04-29-180919168.png)
